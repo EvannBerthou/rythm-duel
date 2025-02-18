7be98ea47c49e32614d51ce81970b08d094ce078
Evann Berthou <evann.berthou@etu.utc.fr>
2025-02-18 18:25:10 +0100

    Speed depends on BPM

--- src/main.c
+++ src/main.c
@@ -6,17 +6,16 @@
 #define CELL_COUNT 4
 #define CELL_BLINK_TIME 0.25f
 #define NOTE_COUNT 16
-#define NOTE_SPEED 600.0f
 
 typedef struct {
   int spawn_tick;
   int column;
   bool spawned;
-  int position;
+  float position;
 } note;
 
-const float BPM = 60.0f;
-const float interval = 60.0f / BPM;
+float BPM = 120.0f;
+float interval; // = 60.0f / BPM;
 float timer = 0;
 bool visible = true;
 
@@ -25,22 +24,18 @@ float global_tick = 0.0f;
 Sound metronome;
 Sound hitsound;
 
-float cells_timer[CELL_COUNT] = {0};
-note notes[NOTE_COUNT] = {0};
-
 const int board_width = 800;
+const int board_height = 750;
 const int cell_padding = 10;
 const int cell_width = (board_width - 10) / CELL_COUNT - cell_padding;
 const int cell_height = 150;
 const int cell_top = 800 - cell_height;
 
-void init(void) {
-  InitAudioDevice();
-  Font loaded_font = GetFontDefault();
-  SetDefaultFont(loaded_font);
-  metronome = LoadSound("sounds/Perc_Snap_hi.wav");
-  hitsound = LoadSound("sounds/Synth_Bell_A_hi.wav");
+float cells_timer[CELL_COUNT] = {0};
+note notes[NOTE_COUNT] = {0};
+float note_speed;
 
+void reset_notes(void) {
   notes[0] = (note){0, 0, 0, 0};
   notes[1] = (note){1, 1, 0, 0};
   notes[2] = (note){1, 2, 0, 0};
@@ -59,9 +54,27 @@ void init(void) {
   notes[15] = (note){12, 0, 0, 0};
 }
 
+void set_bpm(float _bpm) {
+  BPM = _bpm;
+  interval = 60.0f / BPM;
+  note_speed = (float)(board_height - cell_height) / (4.0f * interval);
+}
+
+void init(void) {
+  printf("d=%d\n", board_height - cell_height);
+  printf("f=%f\n", note_speed);
+  InitAudioDevice();
+  Font loaded_font = GetFontDefault();
+  SetDefaultFont(loaded_font);
+  metronome = LoadSound("sounds/Perc_Snap_hi.wav");
+  hitsound = LoadSound("sounds/Synth_Bell_A_hi.wav");
+  set_bpm(60.0f);
+  reset_notes();
+}
+
 void draw_board(void) {
 
-  Rectangle outline = {1200.0f / 2 - 400, 50, board_width, 800 - 50};
+  Rectangle outline = {1200.0f / 2 - 400, 50, board_width, board_height};
   DrawRectangleLinesEx(outline, 5, WHITE);
 
   for (int i = 0; i < CELL_COUNT; i++) {
@@ -76,13 +89,16 @@ void draw_board(void) {
       Color faded_color = ColorAlpha(RED, cells_timer[i] / CELL_BLINK_TIME);
       DrawRectangleRec(inner, faded_color);
     }
+    /*DrawCircle(pos.x + cell_width / 2, 0, 5, GREEN);*/
+    /*DrawCircle(pos.x + cell_width / 2, 50 + board_height - cell_height, 5,*/
+    /*           GREEN);*/
   }
 
   for (int i = 0; i < NOTE_COUNT; i++) {
     if (notes[i].spawned) {
       Rectangle note_pos = {
-          outline.x + (cell_width + cell_padding) * notes[i].column + 10,
-          outline.y + notes[i].position, cell_width, cell_height};
+          outline.x + (cell_width + cell_padding) * notes[i].column + 10 + 5,
+          50 + notes[i].position - 5, cell_width - 10, cell_height - 10};
       DrawRectangleRec(note_pos, BLUE);
     }
   }
@@ -143,11 +159,17 @@ void main_loop(void) {
       notes[i].spawned = true;
     }
     if (notes[i].spawned) {
-      notes[i].position += dt * NOTE_SPEED;
+      notes[i].position += dt * note_speed;
     }
     if (notes[i].position > 800) {
       notes[i].spawned = false;
     }
+  }
+
+  if (global_tick == 15) {
+    set_bpm(BPM * 1.5f);
+    reset_notes();
+    global_tick = 0;
   }
 
   for (int i = 0; i < CELL_COUNT; i++) {
@@ -186,5 +208,6 @@ void main_loop(void) {
   }
 
   DrawFPS(0, 0);
+  DrawText(TextFormat("BPM=%.0f", BPM), 1000, 0, 26, WHITE);
   EndDrawing();
 }
